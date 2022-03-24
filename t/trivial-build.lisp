6a7f475a67538cff400a0386f8b8dc755cba9c32
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2022-03-24 10:46:40 -0500

    Fix windows build

--- t/trivial-build.lisp
+++ t/trivial-build.lisp
@@ -1,30 +1,33 @@
 (in-package :cl-user)
 (defpackage trivial-build-test
-  (:use :cl :fiveam)
-  (:export :main
-           :run-tests))
+	(:use :cl :fiveam)
+	(:export :main
+				:run-tests))
 (in-package :trivial-build-test)
 
 (defun main ()
-  (format t "1"))
+	(format t "1"))
 
 (def-suite tests
-  :description "trivial-build tests.")
+	:description "trivial-build tests.")
 (in-suite tests)
 
 (test simple-test
-  (finishes (main))
-  (let ((path (asdf:system-relative-pathname :trivial-build #p"t/binary")))
-    (finishes
-     (trivial-build:build :trivial-build-test "(trivial-build-test:main)" path))
-    (is-true
-     (probe-file path))
-    (is
-     (equal (string-trim (list #\Newline)
-                         (uiop:run-program (namestring path) :output :string))
-            "1"))
-    (when (probe-file path)
-      (delete-file path))))
+	(finishes (main))
+	(let ((path (asdf:system-relative-pathname
+						:trivial-build
+						(concatenate 'string "t/binary" #+win32 ".exe"))))
+		(finishes
+			(trivial-build:build :trivial-build-test "(trivial-build-test:main)" path))
+		(is-true
+			(probe-file path))
+		(is
+			(equal (string-trim
+							(list #\Newline)
+							(uiop:run-program (namestring path) :output :string))
+						"1"))
+		(when (probe-file path)
+			(delete-file path))))
 
 (defun run-tests ()
-  (run! 'tests))
+	(run! 'tests))
